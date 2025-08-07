d6de7de4e34a5ded05c1f8697ede46ede8f580f2
Perry <perry.olsson@smartsheet.com>
2025-08-06 20:00:51 -0700

    concurrency

--- misc/src/main.rs
+++ misc/src/main.rs
@@ -1,5 +1,14 @@
 mod memory_leak;
 mod weak_ref;
+mod closures;
+mod collection_test;
+mod enums;
+mod pig_latin;
+mod rectangles;
+mod ref_cell;
+mod smart_pointers;
+mod temperature;
+mod terminal_io;
 
 fn main() {
     weak_ref::run2();
