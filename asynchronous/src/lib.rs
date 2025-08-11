642cf9e34e68e12a3ce1038da4f9ebf4ae2ca35b
Perry Olsson <olsson.perry@gmail.com>
2025-08-10 23:09:54 -0700

    More async

--- asynchronous/src/lib.rs
+++ asynchronous/src/lib.rs
@@ -1,11 +1,69 @@
-use trpl::Html;
+use trpl::{Either, Html, Stream, StreamExt, ReceiverStream};
 use std::time::Duration;
 use std::pin::{pin, Pin};
 use std::thread;
 use std::time::Instant;
 
 pub async fn run() {
-    benchmark_sleep_vs_yield().await
+    message_stream().await
+}
+
+async fn message_stream() {
+    let mut messages = pin!(get_messages().timeout(Duration::from_millis(200)));
+
+    while let Some(result) = messages.next().await {
+        match result {
+            Ok(message) => println!("{message}"),
+            Err(reason) => eprintln!("Error with reason {}", reason)
+        }
+    }
+}
+
+fn get_messages() -> impl Stream<Item = String> {
+    let (tx, rx) = trpl::channel();
+    trpl::spawn_task(async move {
+        let messages = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j"];
+        for (index, message) in messages.into_iter().enumerate() {
+            let time_to_sleep= if index % 2 == 0 { 100 } else { 300 };
+            trpl::sleep(Duration::from_millis(time_to_sleep)).await;
+            tx.send(format!("Message: '{message}'")).unwrap();
+        }
+    });
+
+    ReceiverStream::new(rx)
+}
+
+async fn stream_example() {
+    let numbers = vec![1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+    let itr = numbers.iter().map(|n| n*2);
+    let stream = trpl::stream_from_iter(itr);
+    let mut filtered = stream.filter(|n| n % 3 == 0 || n % 5 == 0);
+
+    while let Some(value) = filtered.next().await {
+        println!("{}", value);
+    }
+}
+
+
+async fn timeout_example() {
+    let slow = async {
+        trpl::sleep(Duration::from_millis(100)).await;
+        "I finished"
+    };
+
+    match timeout(slow, Duration::from_millis(200)).await {
+        Ok(message) => println!("Succeeded with '{message}'"),
+        Err(duration) => {
+            println!("Failed after {} seconds", duration.as_secs())
+        }
+    }
+}
+
+async fn timeout<F: Future>(runnable: F, duration: Duration) -> Result<F::Output, Duration> {
+    match trpl::race(runnable, trpl::sleep(duration)).await {
+        Either::Left(v) => Result::Ok(v),
+        Either::Right(_) => Result::Err(duration)
+    }
 }
 
 async fn benchmark_sleep_vs_yield() {
