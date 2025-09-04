561ef8846d29ea10ae05f17f97477896cb829024
Perry Olsson <olsson.perry@gmail.com>
2025-09-04 07:00:24 -0700

    Simple web server

--- hello_web_server/src/main.rs
+++ hello_web_server/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    fs,
     io::{BufReader, prelude::*},
     net::{TcpListener, TcpStream}
 };
@@ -15,13 +16,20 @@ fn main() {
 
 fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&stream);
-    let _http_request: Vec<_> = buf_reader
-        .lines()
-        .map(|result| result.unwrap())
-        .take_while(|line| !line.is_empty())
-        .collect();
+    let req_line = buf_reader.lines().next().unwrap().unwrap();
 
-    let response = "HTTP/1.1 200 OK\r\n\r\nhello";
+    let (status_line, filename) = match req_line.as_str() {
+        "GET / HTTP/1.1" => {
+            ("HTTP/1.1 200 OK", "hello.html")
+        }
+        _ => {
+            ("HTTP/1.1 404 NOT FOUND", "404.html")
+        }
+    };
 
-    stream.write_all(response.as_bytes()).unwrap();
+    let html = fs::read_to_string(filename).unwrap();
+    let len = html.len();
+    let res = format!("{status_line}\r\nContent-Length: {len}\r\n\r\n{html}");
+
+    stream.write_all(res.as_bytes()).unwrap();
 }
